3a8d7cff4f7b1786f9cd40b6d9c1df3aa01c7861
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-24 12:17:16 +0530

    Updates

---
+++ precache-manifest.c915b356609e9757e4071c369f670d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd3a449d8eea89b30f7a8dfa0b70001",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ee2b409800dc59d84c8f",
+    "url": "/portfolio/static/css/main.4beec8f1.chunk.css"
+  },
+  {
+    "revision": "5cdb3585a997f5fb51e4",
+    "url": "/portfolio/static/js/2.49a80343.chunk.js"
+  },
+  {
+    "revision": "ee2b409800dc59d84c8f",
+    "url": "/portfolio/static/js/main.c28293b0.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b7b7cb4bb64fdca254a7d2119770e33e",
+    "url": "/portfolio/static/media/Brix-Sans-Regular.b7b7cb4b.otf"
+  }
+]);
