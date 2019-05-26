48b6157c6f4805b77484200a426c034f3efb79d3
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-27 01:57:46 +0530

    Updates

---
+++ precache-manifest.8aa0a55155cdc39c9e49f0064a54c0db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890915381798bd29838eb659589385c6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5e480285bc4c8ad12952",
+    "url": "/portfolio/static/css/main.18e32a69.chunk.css"
+  },
+  {
+    "revision": "dca86eb5c8b6194dead2",
+    "url": "/portfolio/static/js/2.67bd1447.chunk.js"
+  },
+  {
+    "revision": "5e480285bc4c8ad12952",
+    "url": "/portfolio/static/js/main.08b4bcf9.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b7b7cb4bb64fdca254a7d2119770e33e",
+    "url": "/portfolio/static/media/Brix-Sans-Regular.b7b7cb4b.otf"
+  },
+  {
+    "revision": "0e4005d3074ff0e3329877342844b591",
+    "url": "/portfolio/static/media/BrixSansRegular.0e4005d3.ttf"
+  },
+  {
+    "revision": "4be6cd5b230b89e11fa7215331f79046",
+    "url": "/portfolio/static/media/BrixSansRegular.4be6cd5b.woff2"
+  },
+  {
+    "revision": "c72fb73672e7d9bfc727c6ad94b9bf13",
+    "url": "/portfolio/static/media/BrixSansRegular.c72fb736.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ef26b98bb529161cba8dceb56c188faa.js"
+  "/portfolio/precache-manifest.8aa0a55155cdc39c9e49f0064a54c0db.js"
 );
 
 self.addEventListener('message', (event) => {
