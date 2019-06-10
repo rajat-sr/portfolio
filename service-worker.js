95e3f0b8e1ab3ede8b29576b9d4ffee02019a134
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-10 05:42:32 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3467987a1e31dad66400e5a7819f06dd.js"
+  "/portfolio/precache-manifest.5ce7e023a2b515b6d3f89b41e1fbafee.js"
 );
 
 self.addEventListener('message', (event) => {
